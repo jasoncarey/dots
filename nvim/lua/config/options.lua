90f63f44bfe2a74f7e5c498f357b3dbeaa60d4af
Jason Carey <jasdcarey@gmail.com>
2026-02-01 16:35:00 +1100

    move colorscheme.lua to plugs, add transparency

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,24 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Enable terminal transparency
 vim.opt.termguicolors = true
-
-local function set_transparent_bg()
-  vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
-  vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
-  vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE", ctermbg = "NONE" })
-  vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
-end
-
--- Apply on colorscheme change
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = set_transparent_bg,
-})
-
--- Apply after startup (catches initial colorscheme load)
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = set_transparent_bg,
-})
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -18,21 +18,14 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = function(_, opts)
-      opts.style = "night"
-      opts.transparent = true
-      opts.styles = vim.tbl_deep_extend("force", opts.styles or {}, {
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
         sidebars = "transparent",
         floats = "transparent",
-      })
-
-      opts.on_highlights = function(hl, c)
-        hl.Normal = { bg = "NONE" }
-        hl.NormalFloat = { bg = "NONE" }
-        hl.SignColumn = { bg = "NONE" }
-        hl.EndOfBuffer = { bg = "NONE" }
-      end
-    end,
+      },
+    },
   },
   { "tiagovla/tokyodark.nvim", priority = 1000 },
   {
