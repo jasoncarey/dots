5b8019a0098856d31418df6da78bf248351dc1ae
Jason Carey <jasdcarey@gmail.com>
2026-05-11 13:29:09 +1000

    update fish with pnpm bun nvm

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# pipx (used by uv as well)
+fish_add_path -g "$HOME/.local/bin"
+
+# pnpm
+set -gx PNPM_HOME "$HOME/Library/pnpm"
+fish_add_path -g "$PNPM_HOME"
+
+# bun
+set -gx BUN_INSTALL "$HOME/.bun"
+fish_add_path -g "$BUN_INSTALL/bin"
+
 function __nvm_auto_use --on-variable PWD
     if test -f .nvmrc
         set node_version (string trim (cat .nvmrc))
