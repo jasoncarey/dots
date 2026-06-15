eabee23202d1b5025643fe870334ca0606dbc3aa
Jason Carey <jasdcarey@gmail.com>
2026-06-15 12:39:49 +1000

    remove redundant keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,16 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Exit insert mode with jk
-vim.keymap.set("i", "jk", "<Esc>", {
-  desc = "Exit insert mode",
-  silent = true,
-})
-
 -- Keep cursor centered when paging
 -- vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 -- vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 
+-- Exit insert mode with jk / jj
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode", silent = true })
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode", silent = true })
 
